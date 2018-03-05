5c56ab93e6c8a485e944ab276fadd0c3ced3cf24
rawplutonium <lgithigi@gmail.com>
2018-03-05 09:15:08 +0300

    models

--- serve/models.py
+++ serve/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+	"""docstring for Serve"""
+	name =  models.CharField(max_length=100)
+	clocation = models.CharField(max_length=100)
+class restaurant(models.Model):
+	"""docstring for restaurant"""
+	name = models.CharField(max_length=100)
+	rlocation = models.CharField(max_length=100)
+class Food(models.Model):
+	"""docstring for Food"""
+	title = models.CharField(max_length=100)
+	restaurant = models.ForeignKey(restaurant,on_delete=models.CASCADE)
+	category = models.CharField(max_length=100)
+
+
+
+		
